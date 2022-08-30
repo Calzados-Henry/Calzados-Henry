1fd25525d89e5669f6ae735e7e807b2b87e2458a
Facundo Juliá <facundodirie@gmail.com>
2022-08-30 12:31:06 -0300

    Agregados: Modelo de order y order_details

---
+++ api/src/models/OrderDetails.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) =>{
+    sequelize.define('order_details',{
+        id_order:{
+            type:DataTypes,
+            allowNull:false,
+            references:{
+                model:'orders',
+                key:'id'
+            }
+        },
+        id_product:{
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull: false,
+
+        },
+        image:{
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        gender:{
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        size:{
+            type:DataTypes.STRING,
+            allowNull: false, 
+        },
+        color:{
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        quantity:{
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        season:{
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        price:{
+            type:DataTypes.DECIMAL(10,2),
+            allowNull: false,
+        }
+    });
+}
+
