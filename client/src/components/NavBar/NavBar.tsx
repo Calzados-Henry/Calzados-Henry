0991a6d7f829bc60f34b187576951c689adc5ce6
joelorzet <joelorzt@gmail.com>
2022-09-01 11:20:24 -0300

    refactor/nav-changes

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,109 +1,25 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { styled, alpha } from '@mui/material/styles';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import StoreIcon from '@mui/icons-material/Store';
-import User from '../User/User'
+import StoreIconComponent from '../StoreIconComponent/StoreIconComponent';
+import SearchComponent from '../Search/SearchComponent';
+import SideBarComponent from '../SideBarComponent/SideBarComponent';
+import User from '../User/User';
 
-import {
-  AppBar,
-  Box,
-  IconButton,
-  InputBase,
-  Toolbar,
-  Typography,
-} from '@mui/material';
-
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
+import { AppBar, Box, Toolbar } from '@mui/material';
 
 export default function PrimarySearchAppBar() {
-  const navigate = useNavigate();
-
-  const [search, setSearch] = useState({});
-
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setSearch({ ...search, [e.target.name]: e.target.value });
-  };
-
   return (
     <AppBar position='absolute' color='primary'>
       <Toolbar>
-        <IconButton
-          size='large'
-          edge='start'
-          color='inherit'
-          aria-label='open drawer'
-          sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
-        <Typography
-          variant='h6'
-          noWrap
-          component='div'
-          onClick={()=> navigate('/')}
-          sx={{ display: { xs: 'none', sm: 'block' }, mr: 1, '&:hover': {
-            cursor: 'pointer',
-          } }}>
-          SEHOS
-        </Typography>
-        <StoreIcon />
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder='Search...'
-            name='search'
-            onChange={onChange}
-            inputProps={{ 'aria-label': 'search' }}
-          />
-        </Search>
+        <SideBarComponent />
+
+        <StoreIconComponent />
+
+        <Box sx={{ flexGrow: 1.5 }} />
+
+        <SearchComponent />
 
         <Box sx={{ flexGrow: 1 }} />
-        
-        <User />
 
+        <User />
       </Toolbar>
     </AppBar>
   );
