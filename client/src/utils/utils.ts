75c5be3cd384688480db917d99e97de9bc7a30ab
ruizrodrigo <ruiz.nrodrigo@gmail.com>
2022-09-02 12:21:00 -0300

    add shoppingcomponent

---
+++ client/src/utils/utils.ts
@@ -0,0 +1,33 @@
+import { ProductI } from "../features/product/product.model"
+
+const productsArray: ProductI[] = []
+
+export const setLocalStorage = (product: ProductI): void => {
+    productsArray.push(product)
+    if(productsArray.length) localStorage.setItem('product', JSON.stringify(productsArray))
+    else throw new Error('There are any products in the cart')
+}
+
+export const getLocalStorage = () : ProductI[] => {
+        const getProducts = localStorage.getItem('product')
+        if(!getProducts) throw new Error("There are any products in the cart");
+        else {
+            const products: ProductI[] = JSON.parse(getProducts)
+            return products
+        }
+}
+
+export const removeOneFromLS = (id: number) : void => {
+    const getProducts = localStorage.getItem('product')
+        if(!getProducts) throw new Error("There are any products in the cart");
+        else {
+            const filtered: ProductI[] = JSON.parse(getProducts).filter((product: { id: number }) => product.id !== id)
+            if(!filtered.length) localStorage.clear()
+            localStorage.removeItem('product')
+            localStorage.setItem('product', JSON.stringify(filtered))
+        }
+}
+
+export const clearLocalStorage = () : void => {
+    localStorage.clear()
+}
