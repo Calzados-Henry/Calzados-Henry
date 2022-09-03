30eff53f75f64f34bb2a66ab0c51027f0410e610
Andres Parra <andeveling@gmail.com>
2022-09-03 00:53:07 -0500

    fix(client/):Arregle varios bugs e hice mejoras en los componentes ya existentes

--- client/src/components/ProductDetail/photos/Photos.tsx
+++ client/src/components/ProductDetail/photos/Photos.tsx
@@ -1,28 +1,13 @@
-import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 import { itemData } from '../data';
 import { Box } from '@mui/material';
+import { ProductPartial } from '../../../sehostypes/Product';
+import s from './Photos.module.css';
 
 // https://picsum.photos/500/400
-export default function Photos() {
+export default function Photos({ image, title }: ProductPartial) {
   return (
-    <Box>
-      <Box sx={{ width: '100%' }}>
-        <img
-          src={
-            'https://i.picsum.photos/id/63/500/400.jpg?hmac=AdMb2SfNVtaxiemyVlmc51Rcm0tKGYJfx7MidERexf0'
-          }></img>
-      </Box>
-      <Box>
-        <ImageList sx={{ width: 500, height: 220 }} cols={4} rowHeight={100}>
-          {itemData.map(item => (
-            <ImageListItem key={item.img}>
-              <img src={`${item.img}`} srcSet={`${item.img}`} alt={item.title} loading='lazy' />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </Box>
+    <Box sx={{ width: '100%' }} display='grid' justifyContent={'center'}>
+      <img className={s.image} src={image} alt={title}></img>
     </Box>
   );
 }
