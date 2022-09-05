6792f36bd6a70fff0e4942855730ee2298c96cc7
idalla <idallarizza@gmail.com>
2022-09-05 14:02:51 -0300

    Reducers Filters

--- client/src/sehostypes/Product.ts
+++ client/src/sehostypes/Product.ts
@@ -9,17 +9,25 @@ export interface Images {
   id: string | number;
   image: string;
 }
+export interface Category {
+  id: number,
+  category: string,
+  isActive: boolean
+}
 export interface ProductI {
   id: number;
   name: string;
   price: number;
   description: string;
-  category: string;
+  category: Category;
   images: Images[];
   rating: Rating;
   sold: number;
 }
-
+export interface Pricing {
+  base: number,
+  top: number
+}
 // DTO Data Transfer Object : interfaces creadas a partir de una intefaz para manejo de datos
 export interface RatingPartial extends Partial<Rating> {}
 export interface ProductPartial extends Partial<ProductI> {}
