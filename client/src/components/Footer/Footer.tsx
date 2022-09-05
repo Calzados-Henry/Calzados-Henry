98f9ec29f792f138a31cdeb3fa3395976195398c
joelorzet <joelorzt@gmail.com>
2022-09-05 15:32:03 -0300

    fix/footer-side bar fixes

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,46 +1,45 @@
 import { BottomNavigation, BottomNavigationAction, Box, Paper } from '@mui/material';
 import { Facebook, Instagram, WhatsApp } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
-import {useTheme} from '@mui/material/styles'
+import { useTheme } from '@mui/material/styles';
 
 function Footer() {
-  const navigate = useNavigate()
-  const theme = useTheme()
+  const navigate = useNavigate();
+  const theme = useTheme();
 
   return (
     <>
-    <div style={ {height: theme.mixins.toolbar.minHeight, marginTop: 30 } }></div>
-    <Box sx={{ bgcolor: 'primary.main', textAlign: 'center' }}>
-      
-      <Paper 
-        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, bgcolor: 'inherit' }} 
-        elevation={3}>
-      <BottomNavigation showLabels sx={{ bgcolor: 'inherit' }}>
-          <BottomNavigationAction
-            icon={<Facebook />}
-            onClick={() => {
-              window.open('https://www.facebook.com/profile.php?id=100084956481996');
-              return null;
-            }}
-          />
-          <BottomNavigationAction
-            icon={<Instagram />}
-            onClick={() => {
-              window.open('https://www.instagram.com/sehos22/');
-              return null;
-            }}
-          />
-          <BottomNavigationAction
-            icon={<WhatsApp />}
-            onClick={() => {
-              window.open('https://chat.whatsapp.com/HoXT6nlQFrY58e53eRlKbf');
-              return null;
-            }}
-          />
-        </BottomNavigation>
-        <label>Sehos &reg; {new Date().getFullYear()}</label>
-      </Paper>
-    </Box>
+      <div style={{ height: theme.mixins.toolbar.minHeight, marginTop: 30 }}></div>
+      <Box sx={{ bgcolor: 'primary.main', textAlign: 'center' }}>
+        <Paper
+          sx={{ position: 'sticky', bottom: 0, left: 0, right: 0, bgcolor: 'inherit' }}
+          elevation={3}>
+          <BottomNavigation showLabels sx={{ bgcolor: 'inherit' }}>
+            <BottomNavigationAction
+              icon={<Facebook />}
+              onClick={() => {
+                window.open('https://www.facebook.com/profile.php?id=100084956481996');
+                return null;
+              }}
+            />
+            <BottomNavigationAction
+              icon={<Instagram />}
+              onClick={() => {
+                window.open('https://www.instagram.com/sehos22/');
+                return null;
+              }}
+            />
+            <BottomNavigationAction
+              icon={<WhatsApp />}
+              onClick={() => {
+                window.open('https://chat.whatsapp.com/HoXT6nlQFrY58e53eRlKbf');
+                return null;
+              }}
+            />
+          </BottomNavigation>
+          <label>Sehos &reg; {new Date().getFullYear()}</label>
+        </Paper>
+      </Box>
     </>
   );
 }
