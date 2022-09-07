4c6092d965857c47ae978804489f628d5549dd6c
ruizrodrigo <ruiz.nrodrigo@gmail.com>
2022-09-07 17:38:16 -0300

    avances login/register

--- client/src/features/auth/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { useDispatch } from "react-redux";
-import { useLoginMutation } from "./authApiSlice";
-import { setCredentials } from "./authSlice";
-
-export default function Login() {
-    const dispatch = useDispatch();
-    const [login, {data, isLoading, isSuccess}] = useLoginMutation();
-    
-}
