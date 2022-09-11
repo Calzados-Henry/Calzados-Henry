78cd60fc46f035be5bf8e9d3436c1d19b23a5810
joelorzet <joelorzt@gmail.com>
2022-09-11 19:36:56 -0300

    fix/ api-controllers-searchBar logic changes

--- api/src/controllers/SearchBar.ts
+++ api/src/controllers/SearchBar.ts
@@ -1,19 +1,21 @@
 /* import { Products, Users, Category, Product_details, Color, Images, Sizes } from "../db";
 import { Op } from "sequelize" */
-import axios from "axios"
+import axios from 'axios';
 
 export const getProducts = async (product: string): Promise<any> => {
+	try {
+		if (!product) throw new Error(`there's no any product for` + product);
 
-  if (product) {
-    const productsBeta: Array<any> = await axios.get('http://localhost:3001/products').then(data => data.data)
-    const filtrados: Array<any> = productsBeta.filter(p => p.name.toLowerCase().includes(product.toLowerCase()))
-   
-    if (filtrados.length > 0  ) {
-      const datos = filtrados
-      return datos
-    } 
-    } else {
-      throw new Error(`there's no any product for` + product)
+		const { data }: any = await axios.get('http://localhost:3001/products');
 
-    }
-  }
+		const filtrados: Array<any> = data.filter((p: any) =>
+			p.name.toLowerCase().includes(product.toLowerCase())
+		);
+
+		if (!filtrados) throw new Error(`Theres no coincidence for ${product}`);
+
+		return filtrados;
+	} catch (error: any) {
+		return [];
+	}
+};
