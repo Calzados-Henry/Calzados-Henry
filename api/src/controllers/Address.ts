7e3d235e30780a33ca289693df52007d386375c6
Facundo Tomás Juliá Dirié <facundodirie@gmail.com>
2022-09-17 15:02:13 -0300

    agregando add category

--- api/src/controllers/Address.ts
+++ api/src/controllers/Address.ts
@@ -9,65 +9,65 @@ import { Address, Users } from '../db'
 //}
 
 export const getAddress = async (id: string): Promise<object> => {
-  
-  const userAddresses: any = await Users.findByPk(id, { include: { model: Address } })
-  if (!userAddresses.Addresses && userAddresses) {
-    throw new Error(`There's not any addresses for the user id: ${id}`)
-  } else if (!userAddresses) {
-    throw new Error(`There's not any user for the id: ${id}`)
-  } else {
-    return (userAddresses.Addresses)
-  }
+
+    const userAddresses: any = await Users.findByPk(id, { include: { model: Address } })
+    if (!userAddresses.Addresses && userAddresses) {
+        throw new Error(`There's not any addresses for the user id: ${id}`)
+    } else if (!userAddresses) {
+        throw new Error(`There's not any user for the id: ${id}`)
+    } else {
+        return (userAddresses.Addresses)
+    }
 }
 
 export const postAddress = async (id: string, body: any): Promise<object> => {
-  const { address, zip_code, city, state, country, title }: any = body
-  const [newAddress, created]: any = await Address.findOrCreate({
-    where: {
-      id_user: id,
-      title: title,
-      address: address,
-      city: city,
-      state: state,
-      country: country,
-      zip_code: zip_code
+    const { address, zip_code, city, state, country, title }: any = body
+    const [newAddress, created]: any = await Address.findOrCreate({
+        where: {
+            id_user: id,
+            title: title,
+            address: address,
+            city: city,
+            state: state,
+            country: country,
+            zip_code: zip_code
+        }
+    })
+    const user: any = await Users.findByPk(id)
+    if (user && created) {
+        await user.addAddress(newAddress)
+        const userAddresses = await Users.findByPk(id, { include: { model: Address } })
+        if (userAddresses) {
+            return userAddresses
+        }
+    } else if (!created) {
+        throw new Error(`The address ${newAddress.address} already exists`);
+    } else if (!user) {
+        throw new Error(`We couldn't find user with id: ${id}`)
     }
-  })
-  const user: any = await Users.findByPk(id)
-  if (user && created) {
-    await user.addAddress(newAddress)
-    const userAddresses = await Users.findByPk(id, { include: { model: Address } })
-    if (userAddresses) {
-      return userAddresses
-    }
-  } else if (!created) {
-    throw new Error(`The address ${newAddress.address} already exists`);
-  } else if (!user) {
-    throw new Error(`We couldn't find user with id: ${id}`)
-  }
-  throw new Error('An error has ocurred')
+    throw new Error('An error has ocurred')
 }
 
 export const patchAddress = async (value: any): Promise<object> => {
-  var address: any = await Address.findByPk(value.id)
-  if (address !== null) {
-    if (address.address === value.address && address.zip_code === value.zip_code) {
-      throw new Error(`Please type another address`)
+    var address: any = await Address.findByPk(value.id)
+    if (address !== null) {
+        if (address.address === value.address && address.zip_code === value.zip_code) {
+            throw new Error(`Please type another address`)
+        }
+        address.set(value);
+        await address.save();
+        return address
     }
-    address.set(value);
-    await address.save();
-    return address
-  }
-  throw new Error(`There's not any address with the id: ${value.id}`)
+    throw new Error(`There's not any address with the id: ${value.id}`)
 }
 
 export const deleteAddress = async (value: any): Promise<object> => {
-  const deletedAddress: any = await Address.findByPk(value.id)
-  if (deletedAddress.isActive == false) {
-    throw new Error(`${value.address} is already deleted`)
-  } else {
-    deletedAddress.isActive = false
-    await deletedAddress.save()
-  }
-  return deletedAddress
+    const deletedAddress: any = await Address.findByPk(value.id)
+    if (deletedAddress.isActive == false) {
+        throw new Error(`${value.address} is already deleted`)
+    } else {
+        deletedAddress.isActive = false
+        await deletedAddress.save()
+    }
+    return deletedAddress
 }
