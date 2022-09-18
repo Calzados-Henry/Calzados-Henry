18d1ca7d13cb4903df7127f8f38f32b27ea01165
Gabyfinn <gabrieluzzante@gmail.com>
2022-09-18 02:16:51 -0300

    FixingAdress

--- api/src/controllers/Orders.ts
+++ api/src/controllers/Orders.ts
@@ -1,6 +1,6 @@
 'use strict'
 // se requiere el models
-import { Orders, Orders_details, Users } from '../db';
+import { Orders, Orders_details, Products, Users } from '../db';
 
 
 export const getOrders = async (): Promise<any> => {
@@ -29,17 +29,71 @@ export const getOrders = async (): Promise<any> => {
 export const createOrders = async (value: any): Promise<any> => {
   // si todo esta correcto crea una orden de compra
 
-  const { orders_details, ...restOfOrder } = value
-  const createdOrder: any = await Orders.create(restOfOrder)
-  for (const order_detail of orders_details) {
-    await createdOrder.createOrders_detail(order_detail)
+  const { id_user } = value
+  const user: any = await Users.findByPk(id_user)
+  console.log(user.toJSON())
+  const carts = await user.getCart_details({attributes:{exclude: ["id_user"]}})
+  let orders_details: any = []
+  let total_ammount=0
+
+  for (const cart of carts) { 
+    console.log(cart.toJSON())
+    let size = await cart.getSize({attributes: ["size"]})
+    size = size.toJSON()
+    const product_detail = await cart.getProduct_detail({attributes: ["id","id_product","id_color"]})
+    console.log(product_detail.toJSON())
+    let color = await product_detail.getColor({attributes: ['color']})
+    color = color.toJSON()
+    let image = await product_detail.getImages({attributes:["image"],joinTableAttributes: []})
+    image = image[0].toJSON()
+    let product:any = await Products.findByPk(product_detail.id_product, {attributes: [['id','id_product'],'name','gender','season',['sell_price','price']]})
+    product=product?.toJSON()
+    
+    const order_detail = {
+      ...product,
+      ...image,
+      ...size,
+      ...color,
+      quantity:cart.quantity
+    }
+    total_ammount=total_ammount+(order_detail.price*order_detail.quantity)
+    console.log(order_detail)
+
+    console.log(total_ammount)
+    orders_details.push(order_detail)
   }
 
-  return await Orders.findByPk(createdOrder.id, {
-    include: {
-      model: Orders_details
-    }
-  })
+  /* const order = {
+    id_user,
+    address_user:"",
+    total_ammount,
+    order_state:'Pending'
+  } */
+  // console.log("Carrito:", carts[0].toJSON())
+  // const size = await carts[0].getSize()
+  // console.log("Size:", size.toJSON())
+  // const product_detail = await carts[0].getProduct_detail()
+  // console.log("product_detail:", product_detail.toJSON())
+  // const product = await Products.findByPk(product_detail.id_product)
+  // console.log("Product:",product?.toJSON())
+  //Orders.createOrders_detail()
+
+
+  // const order = {
+  //   id_user,
+  // }
+  return user
+  // const { orders_details, ...restOfOrder } = value
+  // const createdOrder: any = await Orders.create(restOfOrder)
+  // for (const order_detail of orders_details) {
+  //   await createdOrder.createOrders_detail(order_detail)
+  // }
+
+  // return await Orders.findByPk(createdOrder.id, {
+  //   include: {
+  //     model: Orders_details
+  //   }
+  // })
 }
 
 export const updateOrders = async (value: any): Promise<any> => {
