d812112097ff6cdbd4ec282388f115d27a2c682f
ruizrodrigo <ruiz.nrodrigo@gmail.com>
2022-09-19 10:54:32 -0300

    fixes

--- client/src/components/OnlinePays/StripePay/StripePay.tsx
+++ client/src/components/OnlinePays/StripePay/StripePay.tsx
@@ -10,16 +10,22 @@ import axios from 'axios';
 import './StripePay.css';
 import Swal from 'sweetalert2';
 import { useAuth } from '@/hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
+import { PublicRoutes } from '@/routes/routes';
+import { useDispatch } from 'react-redux';
+import { getApiUserCart } from '@/features/cart/cartApiSlice';
 
 const stripePromise = loadStripe(stripePublicKey);
 
 // 5200828282828210 card / 3 digitos / fecha futura
 /* OTHER COMPONENT */
 export const CheckoutForm = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const stripe = useStripe();
   const elements = useElements();
   const [loading, setLoading] = useState(false);
-  const { user, token } = useAuth();
+  const { user, token, id } = useAuth();
   const deliveryAddress = JSON.parse(window.localStorage.getItem('deliveryAddress') as string)
 
   /*  */
@@ -33,12 +39,17 @@ export const CheckoutForm = () => {
         title: 'Your payment was received successfully',
         confirmButtonColor: '#412800',
         showConfirmButton: true,
+        timer: 1600
       });
       axios.post('http://localhost:3001/email', {
         email: user,
         subject: 'Sehos Store Purchase',
         content: 'Your payment was received successfully! ',
       });
+      dispatch(getApiUserCart(id))
+      setTimeout(() => {
+        navigate(PublicRoutes.home)
+      }, 2000);
     };
 
     
@@ -58,8 +69,9 @@ export const CheckoutForm = () => {
               'Authorization': `bearer ${token}`
           }
         });
-        data && data.status === 'succeeded'
-          ? successPayment()
+        data && status === 200
+          ? 
+          successPayment()
           : Swal.fire({
               icon: 'error',
               title: 'Your payment was declined',
@@ -67,11 +79,11 @@ export const CheckoutForm = () => {
               confirmButtonColor: '#412800',
               showConfirmButton: true,
             });
-      } catch (error) {
+      } catch (error: any) {
         Swal.fire({
           icon: 'error',
           title: 'Your payment was declined',
-          text: 'Contact your bank and try again',
+          text: error.message,
           confirmButtonColor: '#412800',
           showConfirmButton: true,
         });
